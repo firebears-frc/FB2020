045d16b8d0e18a8e23767d489370d76931ea8dee
Nicholas S <nschense@gmail.com>
2020-01-25 14:48:03 -0600

    Added Shooter PID and Motor CAN IDs to config file

--- src/main/java/org/firebears/subsystems/Shooter.java
+++ src/main/java/org/firebears/subsystems/Shooter.java
@@ -4,27 +4,33 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends SubsystemBase {
 
+    static final Preferences config = Preferences.getInstance();
+
     static private final int PID_LOOP_IDX = 0;
-    static private final int TIMEOUT_MS = 30;
-    static private final double P = 1.0;
-    static private final double I = 0.0;
-    static private final double D = 0.0;
-    static private final double F = 0.0;
+    static private final int TIMEOUT_MS = config.getInt("shooter.timeout", 30);
+    static private final double P = config.getDouble("shooter.P", 1.0);
+    static private final double I = config.getDouble("shooter.I", 0.0);
+    static private final double D = config.getDouble("shooter.D", 0.0);
+    static private final double F = config.getDouble("shooter.F", 0.0);
     static private final double RPM = 350.0 * 5.0;
     static private final double SENSOR_UNITS_PER_REV = 4096;
     static private final double GEAR_RATIO = 13.56;
     static private final double PER_MINUTE_100_MS = 600.0;
 
+
+    
+    
     // private final WPI_TalonSRX srx;
     private final TalonSRX srx;
 
     public Shooter() {
         super();
-        srx = new TalonSRX(12);
+        srx = new TalonSRX(config.getInt("shooter.motor1", 12));
         srx.configFactoryDefault();
         srx.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,
                                          PID_LOOP_IDX,
