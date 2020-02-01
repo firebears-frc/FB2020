8285be73b8b79ee5f2c008a78f31cb55dec176bf
Nicholas S <nschense@gmail.com>
2020-02-01 13:41:06 -0600

    Added trapazoidal drive command

---
+++ src/main/java/org/firebears/commands/TrapazoidalDriveCommand.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.firebears.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class TrapazoidalDriveCommand extends CommandBase {
+  public TrapazoidalDriveCommand() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  public void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  public void execute() {
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+}
