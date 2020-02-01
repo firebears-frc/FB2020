e54e754b74f0f64fb3cf20bc05e7dc9eb8f4e5df
Nicholas S <nschense@gmail.com>
2020-02-01 12:53:24 -0600

    Removed a bunch of unused comments from the subsytems and commands

--- src/main/java/org/firebears/commands/IndexShootingCommand.java
+++ src/main/java/org/firebears/commands/IndexShootingCommand.java
@@ -1,9 +1,9 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+
+
+
+
+
+
 
 package org.firebears.commands;
 
@@ -20,13 +20,13 @@ public class IndexShootingCommand extends CommandBase {
     addRequirements(Robot.storage, Robot.shooter);
   }
 
-  // Called when the command is initially scheduled.
+  
   @Override
   public void initialize() {
     ballQueue = new BallQueueCommand();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
+  
   @Override
   public void execute() {
     if (Robot.shooter.isWheelSpunUp()) {
@@ -35,12 +35,12 @@ public class IndexShootingCommand extends CommandBase {
     }
   }
 
-  // Called once the command ends or is interrupted.
+  
   @Override
   public void end(boolean interrupted) {
   }
 
-  // Returns true when the command should end.
+  
   @Override
   public boolean isFinished() {
     if (ballsShot == 5) {
--- src/main/java/org/firebears/subsystems/Storage.java
+++ src/main/java/org/firebears/subsystems/Storage.java
@@ -47,7 +47,7 @@ public class Storage extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    
   }
 
   public void transfer() {
