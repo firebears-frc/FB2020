d215ab8932532ebe49256a87205efa2b0dd69966
DougLau <doug.p.lau@gmail.com>
2020-02-07 19:43:42 -0600

    Cleaned up storage subsystem

--- src/main/java/org/firebears/subsystems/Storage.java
+++ src/main/java/org/firebears/subsystems/Storage.java
@@ -11,43 +11,42 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Storage extends SubsystemBase {
-  private CANSparkMax indexMotor;
-  private WPI_TalonSRX belt1Motor;
-  private WPI_TalonSRX belt2Motor;
-  private SpeedControllerGroup belts;
+    private CANSparkMax indexMotor;
+    private WPI_TalonSRX belt1Motor;
+    private WPI_TalonSRX belt2Motor;
+    private SpeedControllerGroup belts;
 
-  private DigitalInput positionSensor;
-  private DigitalInput loadEye;
-  private DigitalInput eye1;
-  private DigitalInput eye2;
-  private DigitalInput eye3;
-  private DigitalInput eye4;
-  private DigitalInput eye5;
+    private DigitalInput positionSensor;
+    private DigitalInput loadEye;
+    private DigitalInput eye1;
+    private DigitalInput eye2;
+    private DigitalInput eye3;
+    private DigitalInput eye4;
+    private DigitalInput eye5;
 
-  final Preferences config = Preferences.getInstance();
+    private final Preferences config = Preferences.getInstance();
 
-  public Storage() {
-    belt1Motor = new WPI_TalonSRX(config.getInt("storage.belt1Motor.canID", 16)); // PDP channel 0
-    belt2Motor = new WPI_TalonSRX(config.getInt("storage.belt2Motor.canID", 15)); // PDP channel 1
-    belts = new SpeedControllerGroup(belt1Motor, belt2Motor);
+    public Storage() {
+        belt1Motor = new WPI_TalonSRX(config.getInt("storage.belt1Motor.canID", 16)); // PDP channel 0
+        belt2Motor = new WPI_TalonSRX(config.getInt("storage.belt2Motor.canID", 15)); // PDP channel 1
+        belts = new SpeedControllerGroup(belt1Motor, belt2Motor);
 
-    int indexMotorCanID = config.getInt("storage.indexMotor.canID", 14);
-    indexMotor = new CANSparkMax(indexMotorCanID, MotorType.kBrushless);
-    indexMotor.setInverted(false);
+        int indexMotorCanID = config.getInt("storage.indexMotor.canID", 14);
+        indexMotor = new CANSparkMax(indexMotorCanID, MotorType.kBrushless);
+        indexMotor.setInverted(false);
 
-    positionSensor = new DigitalInput(config.getInt("storage.position.dio", 4));
-    loadEye = new DigitalInput(config.getInt("storage.loadEye.dio", 5));
-    eye1 = new DigitalInput(config.getInt("storage.eye1.dio", 6));
-    eye2 = new DigitalInput(config.getInt("storage.eye2.dio", 7));
-    eye3 = new DigitalInput(config.getInt("storage.eye3.dio", 8));
-    eye4 = new DigitalInput(config.getInt("storage.eye4.dio", 9));
-    eye5 = new DigitalInput(config.getInt("storage.eye5.dio", 10));
-
+        positionSensor = new DigitalInput(config.getInt("storage.position.dio", 4));
+        loadEye = new DigitalInput(config.getInt("storage.loadEye.dio", 5));
+        eye1 = new DigitalInput(config.getInt("storage.eye1.dio", 6));
+        eye2 = new DigitalInput(config.getInt("storage.eye2.dio", 7));
+        eye3 = new DigitalInput(config.getInt("storage.eye3.dio", 8));
+        eye4 = new DigitalInput(config.getInt("storage.eye4.dio", 9));
+        eye5 = new DigitalInput(config.getInt("storage.eye5.dio", 10));
   }
 
   @Override
   public void periodic() {
-    
+
   }
 
   public void transfer() {
@@ -65,5 +64,4 @@ public class Storage extends SubsystemBase {
   public boolean getPositionSensor() {
     return positionSensor.get();
   }
-
 }
