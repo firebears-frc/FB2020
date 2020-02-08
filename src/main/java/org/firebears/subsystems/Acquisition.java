342b3607e17e69e05cbfea1643a165731d1816a0
DougLau <doug.p.lau@gmail.com>
2020-02-07 19:03:05 -0600

    Cleaned up acquisition subsystem

--- src/main/java/org/firebears/subsystems/Acquisition.java
+++ src/main/java/org/firebears/subsystems/Acquisition.java
@@ -1,38 +1,22 @@
-
 package org.firebears.subsystems;
 
-//import org.firebears.commands.*;
-//import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-//import edu.wpi.first.wpilibj2.command.Subsystem;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import edu.wpi.first.wpilibj.PIDOutput;
-//import edu.wpi.first.wpilibj.PIDSource;
 import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-
-
-
-
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Acquisition extends SubsystemBase {
 
     static final Preferences config = Preferences.getInstance();
 
-    private CANSparkMax lower;
-    private CANSparkMax spin;
-    
-    
-    
+    /** Motor to lower acquisition system */
+    private final CANSparkMax lower;
+
+    /** Motor to spin the stars */
+    private final CANSparkMax spin;
 
     public Acquisition() {
-
-        
-
         int acquisitionLowerCanID = config.getInt("acquisition.lower.canID", 2);
         lower = new CANSparkMax(acquisitionLowerCanID, MotorType.kBrushless);
         lower.setInverted(false);
@@ -40,32 +24,24 @@ public class Acquisition extends SubsystemBase {
         int acquisitionSpinCanID = config.getInt("acquisition.spin.canID", 6);
         spin = new CANSparkMax(acquisitionSpinCanID, MotorType.kBrushless);
         spin.setInverted(false);
-        
     }
 
+    /** Periodic update */
     @Override
     public void periodic() {
         SmartDashboard.putNumber("lowerSpeed", lower.get());
         SmartDashboard.putNumber("spinSpeed", spin.get());
-
-        
-
     }
 
+    /** Start acquiring power cells */
     public void startAcquire() {
         lower.set(1);
         spin.set(1);
     }
 
+    /** Stop acquiring power cells */
     public void endAcquire() {
         lower.set(-1);
-        spin.set(-1);
+        spin.set(0);
     }
-    
-
-    
-
-    
-    
-
 }
