8cb5ef85990b6b10013d7facfc45d1522726dd17
DougLau <doug.p.lau@gmail.com>
2020-02-16 13:11:01 -0600

    Improved loader current limiting

--- src/main/java/org/firebears/subsystems/Loader.java
+++ src/main/java/org/firebears/subsystems/Loader.java
@@ -14,16 +14,26 @@ public class Loader extends SubsystemBase {
     private final WPI_TalonSRX rightBeltMotor;
     private final SpeedControllerGroup belts;
     private final DigitalInput loadEye;
-    private final int TIMEOUT_MS = config.getInt("srx.timeout", 30);
 
     public Loader() {
-        leftBeltMotor = new WPI_TalonSRX(config.getInt("loader.leftBeltMotor.canID", 20));
-        leftBeltMotor.configPeakCurrentLimit(15, TIMEOUT_MS);
-        leftBeltMotor.configPeakCurrentDuration(5000, TIMEOUT_MS);
-        leftBeltMotor.configContinuousCurrentLimit(10,TIMEOUT_MS);
-        rightBeltMotor = new WPI_TalonSRX(config.getInt("loader.rightBeltMotor.canID", 21));
+        int timeoutMs = config.getInt("srx.timeout", 30);
+        int leftBeltCanID = config.getInt("loader.leftBeltMotor.canID", 20);
+        int rightBeltCanID = config.getInt("loader.rightBeltMotor.canID", 21);
+        int eyeDio = config.getInt("loader.eye.dio", 5);
+        int peakCurrentLimit = config.getInt("loader.peakCurrentLimit", 15);
+        int peakCurrentDuration = config.getInt("loader.peakCurrentDuration", 5000);
+        int continuousCurrentLimit = config.getInt("loader.continuousCurrentLimit", 10);
+
+        leftBeltMotor = new WPI_TalonSRX(leftBeltCanID);
+        leftBeltMotor.configPeakCurrentLimit(peakCurrentLimit, timeoutMs);
+        leftBeltMotor.configPeakCurrentDuration(peakCurrentDuration, timeoutMs);
+        leftBeltMotor.configContinuousCurrentLimit(continuousCurrentLimit, timeoutMs);
+        rightBeltMotor = new WPI_TalonSRX(rightBeltCanID);
+        rightBeltMotor.configPeakCurrentLimit(peakCurrentLimit, timeoutMs);
+        rightBeltMotor.configPeakCurrentDuration(peakCurrentDuration, timeoutMs);
+        rightBeltMotor.configContinuousCurrentLimit(continuousCurrentLimit, timeoutMs);
         belts = new SpeedControllerGroup(leftBeltMotor, rightBeltMotor);
-        loadEye = new DigitalInput(config.getInt("loader.eye.dio", 5));
+        loadEye = new DigitalInput(eyeDio);
     }
 
     @Override
