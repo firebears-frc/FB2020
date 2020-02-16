ebccac464ddd734c391c6501330777c7e14c2f2e
DougLau <doug.p.lau@gmail.com>
2020-02-16 13:10:16 -0600

    Formatting

--- src/main/java/org/firebears/commands/CelebrateCommand.java
+++ src/main/java/org/firebears/commands/CelebrateCommand.java
@@ -1,44 +1,34 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.firebears.commands;
-
-import java.lang.module.ModuleDescriptor.Requires;
 
 import org.firebears.Robot;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class CelebrateCommand extends CommandBase {
-  /**
-   * Creates a new CelebrateCommand.
-   */
-  public CelebrateCommand() {
-    addRequirements(Robot.lights);
-  }
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    Robot.lights.celebrate(true);
-  }
+    /** Creates a new CelebrateCommand */
+    public CelebrateCommand() {
+        addRequirements(Robot.lights);
+    }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        Robot.lights.celebrate(true);
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
