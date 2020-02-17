f67d8fe028e91dbd3b44bfba99ee7b3403c031e2
Doug Lau <doug.p.lau@gmail.com>
2020-02-17 11:11:57 -0600

    Improved shooter configuration.

--- src/main/java/org/firebears/subsystems/Shooter.java
+++ src/main/java/org/firebears/subsystems/Shooter.java
@@ -9,43 +9,50 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends SubsystemBase {
 
-    static private final Preferences config = Preferences.getInstance();
-
     static private final int PID_LOOP_IDX = 0;
-    static private final int TIMEOUT_MS = config.getInt("srx.timeout", 30);
-    static private final double P = config.getDouble("shooter.P", 1.0);
-    static private final double I = config.getDouble("shooter.I", 0.0);
-    static private final double D = config.getDouble("shooter.D", 0.0);
-    static private final double F = config.getDouble("shooter.F", 0.0);
     static private final double RPM = 350.0 * 5.0;
     static private final double SENSOR_UNITS_PER_REV = 4096;
     static private final double GEAR_RATIO = 13.56;
     static private final double PER_MINUTE_100_MS = 600.0;
 
-    double targetVelocity = RPM * SENSOR_UNITS_PER_REV / (PER_MINUTE_100_MS * GEAR_RATIO);
+    private final double targetVelocity = RPM * SENSOR_UNITS_PER_REV /
+        (PER_MINUTE_100_MS * GEAR_RATIO);
+
+    private final Preferences config = Preferences.getInstance();
 
     private final TalonSRX srx;
 
     public Shooter() {
         super();
+        double timeoutMs = config.getInt("srx.timeout", 30);
+        int peakCurrentLimit = config.getInt("shooter.peakCurrentLimit", 25);
+        int peakCurrentDuration = config.getInt("shooter.peakCurrentDuration",
+            2000);
+        int continuousCurrentLimit = config.getInt(
+            "shooter.continuousCurrentLimit", 10);
+        double p = config.getDouble("shooter.P", 1.0);
+        double i = config.getDouble("shooter.I", 0.0);
+        double d = config.getDouble("shooter.D", 0.0);
+        double f = config.getDouble("shooter.F", 0.0);
         srx = new TalonSRX(config.getInt("shooter.motor1", 25));
         srx.configFactoryDefault();
-        srx.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, PID_LOOP_IDX, TIMEOUT_MS);
+        srx.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,
+            PID_LOOP_IDX, timeoutMs);
         srx.setSensorPhase(true);
         // Configure nominal / peak outputs
-        srx.configNominalOutputForward(0, TIMEOUT_MS);
-        srx.configNominalOutputReverse(0, TIMEOUT_MS);
-        srx.configPeakOutputForward(1, TIMEOUT_MS);
-        srx.configPeakOutputReverse(1, TIMEOUT_MS);
+        srx.configNominalOutputForward(0, timeoutMs);
+        srx.configNominalOutputReverse(0, timeoutMs);
+        srx.configPeakOutputForward(1, timeoutMs);
+        srx.configPeakOutputReverse(1, timeoutMs);
         // Configure limits
-        srx.configPeakCurrentLimit(25, TIMEOUT_MS);
-        srx.configPeakCurrentDuration(2000, TIMEOUT_MS);
-        srx.configContinuousCurrentLimit(10, TIMEOUT_MS);
+        srx.configPeakCurrentLimit(peakCurrentLimit, timeoutMs);
+        srx.configPeakCurrentDuration(peakCurrentDuration, timeoutMs);
+        srx.configContinuousCurrentLimit(continuousCurrentLimit, timeoutMs);
         // Config PIDF
-        srx.config_kP(PID_LOOP_IDX, P, TIMEOUT_MS);
-        srx.config_kI(PID_LOOP_IDX, I, TIMEOUT_MS);
-        srx.config_kD(PID_LOOP_IDX, D, TIMEOUT_MS);
-        srx.config_kF(PID_LOOP_IDX, F, TIMEOUT_MS);
+        srx.config_kP(PID_LOOP_IDX, p, timeoutMs);
+        srx.config_kI(PID_LOOP_IDX, i, timeoutMs);
+        srx.config_kD(PID_LOOP_IDX, d, timeoutMs);
+        srx.config_kF(PID_LOOP_IDX, f, timeoutMs);
     }
 
     public void periodic() {
