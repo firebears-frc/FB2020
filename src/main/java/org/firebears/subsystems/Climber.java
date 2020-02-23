e69bb1c77ac4eb33180094f327375ce884db0736
DougLau <doug.p.lau@gmail.com>
2020-02-23 08:56:34 -0600

    Disable forward/reverse soft limits for Climber

--- src/main/java/org/firebears/subsystems/Climber.java
+++ src/main/java/org/firebears/subsystems/Climber.java
@@ -15,14 +15,16 @@ public class Climber extends SubsystemBase {
         int peakCurrentLimit = config.getInt("climber.peakCurrentLimit", 40);
         int peakCurrentDuration = config.getInt("climber.peakCurrentDuration",
             3000);
-    int continuousCurrentLimit = config.getInt(
+        int continuousCurrentLimit = config.getInt(
            "climber.continuousCurrentLimit", 20);
         int canID = config.getInt(configCANID, defCANID);
         srx = new WPI_TalonSRX(canID);
+        srx.configForwardSoftLimitEnable(false, timeoutMs);
+        srx.configReverseSoftLimitEnable(false, timeoutMs);
         srx.setInverted(false);
         srx.configPeakCurrentLimit(peakCurrentLimit, timeoutMs);
         srx.configPeakCurrentDuration(peakCurrentDuration, timeoutMs);
-       srx.configContinuousCurrentLimit(continuousCurrentLimit,
+        srx.configContinuousCurrentLimit(continuousCurrentLimit,
             timeoutMs);
 
         addChild(configCANID, srx);
@@ -33,16 +35,14 @@ public class Climber extends SubsystemBase {
     }
 
     public void extend() {
-       // srx.setInverted(false);
         srx.set(ControlMode.PercentOutput, 0.5);
     }
 
     public void retract() {
-        //srx should be spelled sux
-        //srx.setInverted(true);
         srx.set(ControlMode.PercentOutput, -0.5);
     }
-    public void stop(){
+
+    public void stop() {
         srx.set(ControlMode.PercentOutput, 0);
     }
 }
