7ccba239023345c2dc95279d627fec432875f63a
DougLau <doug.p.lau@gmail.com>
2020-02-29 13:47:57 -0600

    Add default climber type

--- src/main/java/org/firebears/Robot.java
+++ src/main/java/org/firebears/Robot.java
@@ -84,7 +84,7 @@ public class Robot extends TimedRobot {
     }
 
     private Climber createClimber(String configCANID, int defCANID) {
-        if (config.getString("climber.type", "").equals("srx")) {
+        if (config.getString("climber.type", "srx").equals("srx")) {
             return new ClimberSRX(configCANID, defCANID);
         } else {
             return new ClimberSparkMAX(configCANID, defCANID);
