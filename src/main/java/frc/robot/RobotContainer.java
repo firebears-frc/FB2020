13f51d42382c56049f9db48e1fdc1f72de9ca76b
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2023-11-23 09:27:23 -0600

    Remove old code, place in example

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,68 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.Chassis;
-import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.Indexer;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
 public class RobotContainer {
-    public class Constants {
-        public static final int CONTROLLER_PORT = 0;
-    }
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-    private final Chassis chassis;
-    private final Shooter shooter;
-    private final Intake intake;
-    private final Feeder feeder;
-    private final Indexer indexer;
-    private final CommandXboxController controller;
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
-    public RobotContainer() {
-        chassis = new Chassis();
-        shooter = new Shooter();
-        intake = new Intake();
-        feeder = new Feeder();
-        indexer = new Indexer();
-        controller = new CommandXboxController(Constants.CONTROLLER_PORT);
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
 
-        configureBindings();
-        displayGitInfo();
-    }
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    private void configureBindings() {
-        // Chassis
-        chassis.setDefaultCommand(chassis.defaultCommand(controller::getLeftY, controller::getLeftX));
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
 
-        // Intake & Feeder
-        controller.leftTrigger().whileTrue(intake.intake()).whileTrue(feeder.run());
-        controller.leftBumper().whileTrue(intake.reverse()).whileTrue(feeder.reverse());
-
-        // Indexer
-        controller.a().whileTrue(indexer.run());
-        controller.b().whileTrue(indexer.reverse());
-
-        // Shooter
-        controller.rightTrigger().whileTrue(shooter.shoot());
-        controller.rightBumper().whileTrue(shooter.reverse());
-    }
-
-    public Command getAutonomousCommand() {
-        return null;
-    }
-
-    /**
-     * Display the git info on the dashboard via network tables
-     */
-    private void displayGitInfo() {
-        final NetworkTable table = NetworkTableInstance.getDefault().getTable("Build Info");
-        table.getEntry("Branch Name").setValue(BuildConstants.GIT_BRANCH);
-        table.getEntry("Commit Hash (Short)").setValue(BuildConstants.GIT_SHA.substring(0, 8));
-        table.getEntry("Commit Hash (Full)").setValue(BuildConstants.GIT_SHA);
-        table.getEntry("Build Time").setValue(BuildConstants.BUILD_DATE);
-    }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
 }
