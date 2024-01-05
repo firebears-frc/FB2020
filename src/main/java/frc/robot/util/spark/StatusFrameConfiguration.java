3fc0524a69576de989f1bdd54732c93ef8bf0a0a
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2024-01-05 08:17:05 -0600

    Add default status frame config

--- src/main/java/frc/robot/util/spark/StatusFrameConfiguration.java
+++ src/main/java/frc/robot/util/spark/StatusFrameConfiguration.java
@@ -9,64 +9,70 @@ public interface StatusFrameConfiguration {
     // https://docs.revrobotics.com/sparkmax/operating-modes/control-interfaces#periodic-status-frames
     public void apply(CANSparkBase motor);
 
-    public static StatusFrameConfiguration normal() {
-        return new StatusFrameConfiguration() {
-            private static final Map<PeriodicFrame, Integer> NORMAL_FRAME_CONFIGURATION = Map.of(
-                    PeriodicFrame.kStatus0, 20,
-                    PeriodicFrame.kStatus1, 20,
-                    PeriodicFrame.kStatus2, 20,
-                    PeriodicFrame.kStatus3, 1000,
-                    PeriodicFrame.kStatus4, 1000,
-                    PeriodicFrame.kStatus5, 1000,
-                    PeriodicFrame.kStatus6, 1000);
+    public static StatusFrameConfiguration defaultStatusFrameConfiguration() {
+        return create(Map.of(
+                PeriodicFrame.kStatus0, 10,
+                PeriodicFrame.kStatus1, 20,
+                PeriodicFrame.kStatus2, 20,
+                PeriodicFrame.kStatus3, 50,
+                PeriodicFrame.kStatus4, 20,
+                PeriodicFrame.kStatus5, 200,
+                PeriodicFrame.kStatus6, 200));
+    }
 
-            @Override
-            public void apply(CANSparkBase motor) {
-                StatusFrameConfiguration.apply(motor, NORMAL_FRAME_CONFIGURATION);
-            }
-        };
+    public static StatusFrameConfiguration normal() {
+        return create(Map.of(
+                PeriodicFrame.kStatus0, 20,
+                PeriodicFrame.kStatus1, 20,
+                PeriodicFrame.kStatus2, 20,
+                PeriodicFrame.kStatus3, 1000,
+                PeriodicFrame.kStatus4, 1000,
+                PeriodicFrame.kStatus5, 1000,
+                PeriodicFrame.kStatus6, 1000));
+    }
+
+    public static StatusFrameConfiguration leader() {
+        return create(Map.of(
+                PeriodicFrame.kStatus0, 1,
+                PeriodicFrame.kStatus1, 20,
+                PeriodicFrame.kStatus2, 20,
+                PeriodicFrame.kStatus3, 1000,
+                PeriodicFrame.kStatus4, 1000,
+                PeriodicFrame.kStatus5, 1000,
+                PeriodicFrame.kStatus6, 1000));
     }
 
     public static StatusFrameConfiguration absoluteEncoder() {
-        return new StatusFrameConfiguration() {
-            private static final Map<PeriodicFrame, Integer> ABSOLUTE_ENCODER_CONFIGURATION = Map.of(
-                    PeriodicFrame.kStatus0, 20,
-                    PeriodicFrame.kStatus1, 20,
-                    PeriodicFrame.kStatus2, 20,
-                    PeriodicFrame.kStatus3, 1000,
-                    PeriodicFrame.kStatus4, 1000,
-                    PeriodicFrame.kStatus5, 20,
-                    PeriodicFrame.kStatus6, 1000);
-
-            @Override
-            public void apply(CANSparkBase motor) {
-                StatusFrameConfiguration.apply(motor, ABSOLUTE_ENCODER_CONFIGURATION);
-            }
-        };
+        return create(Map.of(
+                PeriodicFrame.kStatus0, 20,
+                PeriodicFrame.kStatus1, 20,
+                PeriodicFrame.kStatus2, 20,
+                PeriodicFrame.kStatus3, 1000,
+                PeriodicFrame.kStatus4, 1000,
+                PeriodicFrame.kStatus5, 20,
+                PeriodicFrame.kStatus6, 1000));
     }
 
-    public static StatusFrameConfiguration leadingAbsoluteEncoder() {
-        return new StatusFrameConfiguration() {
-            private static final Map<PeriodicFrame, Integer> LEADING_ABSOLUTE_ENCODER_CONFIGURATION = Map.of(
-                    PeriodicFrame.kStatus0, 1,
-                    PeriodicFrame.kStatus1, 20,
-                    PeriodicFrame.kStatus2, 20,
-                    PeriodicFrame.kStatus3, 1000,
-                    PeriodicFrame.kStatus4, 1000,
-                    PeriodicFrame.kStatus5, 20,
-                    PeriodicFrame.kStatus6, 1000);
-
-            @Override
-            public void apply(CANSparkBase motor) {
-                StatusFrameConfiguration.apply(motor, LEADING_ABSOLUTE_ENCODER_CONFIGURATION);
-            }
-        };
+    public static StatusFrameConfiguration absoluteEncoderLeader() {
+        return create(Map.of(
+                PeriodicFrame.kStatus0, 1,
+                PeriodicFrame.kStatus1, 20,
+                PeriodicFrame.kStatus2, 20,
+                PeriodicFrame.kStatus3, 1000,
+                PeriodicFrame.kStatus4, 1000,
+                PeriodicFrame.kStatus5, 20,
+                PeriodicFrame.kStatus6, 1000));
     }
 
-    private static void apply(CANSparkBase motor, Map<PeriodicFrame, Integer> periods) {
-        for (Map.Entry<PeriodicFrame, Integer> entry : periods.entrySet()) {
-            Util.configure(period -> motor.setPeriodicFramePeriod(entry.getKey(), entry.getValue()), entry.getValue(),
-                    entry.getKey().name());
-        }
+    private static StatusFrameConfiguration create(Map<PeriodicFrame, Integer> periods) {
+        return new StatusFrameConfiguration() {
+            @Override
+            public void apply(CANSparkBase motor) {
+                for (Map.Entry<PeriodicFrame, Integer> entry : periods.entrySet()) {
+                    Util.configure(period -> motor.setPeriodicFramePeriod(entry.getKey(), entry.getValue()),
+                            entry.getValue(), entry.getKey().name());
+                }
+            }
+        };
     }
 }
